325936cd92c18b1599c99c91a154d4b12e4747ca
Marcin Jeliński <marcin.jelinski@nordicsemi.no>
2022-03-03 12:52:46 +0100

    scripts: sbom: Parallel detection for scancode-toolkit
    Signed-off-by: Marcin Jeliński <marcin.jelinski@nordicsemi.no>

--- scripts/west_commands/sbom/scancode_toolkit_detector.py
+++ scripts/west_commands/sbom/scancode_toolkit_detector.py
@@ -3,22 +3,76 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
+from concurrent.futures import thread
 from itertools import count
 import subprocess
 import json
 import multiprocessing
+from threading import Thread
 from types import SimpleNamespace
-from data_structure import Data
+from queue import Queue
+from data_structure import Data, FileInfo
 from args import args
 
+class _FileQueue(Queue):
+    SENTINEL = object()
 
-def cpu_count():
+    def close(self):
+        self.put(self.SENTINEL)
+
+    def __iter__(self):
+        while True:
+            item = self.get()
+            try:
+                if item is self.SENTINEL:
+                    return
+                yield item
+            finally:
+                self.task_done()
+
+
+class _Worker(Thread):
+    def __init__(self, func, in_queue):
+        super().__init__()
+        self.func = func
+        self.in_queue = in_queue
+    
+    def run(self):
+        for item in self.in_queue:
+            result = self.func(item)
+
+
+def _cpu_count():
     '''Retrunt the cpu count'''
     try:
         count = multiprocessing.cpu_count()
     except:
         count = 1
     return count
+
+
+def _run_scancode(file: FileInfo):
+    result = subprocess.run(['scancode', '-cl',
+                            '--json-pp', '-',
+                            '--license-text',
+                            '--license-text-diagnostics',
+                            file.file_path],
+                            capture_output=True,
+                            encoding='utf-8')
+    try:
+        result.check_returncode()
+    except:
+        file.errors.append(f'scancode_toolkit: {result.stderr}')
+    else:
+        result_dict = json.loads(result.stdout)
+        licenses = set()
+        for i in result_dict['files'][0]['licenses']:
+            if i['key'] == 'unknown-spdx':
+                licenses.add(i['matched_text'].replace('SPDX-License-Identifier: ', ''))
+            else:
+                licenses.add(i['name'])
+        file.licenses = file.licenses.union(licenses)
+        file.detectors.add('scancode_toolkit')
 
 
 def detect_cache(data: Data, optional: bool):
@@ -40,31 +94,21 @@ def detect_cache(data: Data, optional: bool):
 
 
 def detect(data: Data, optional: bool):
+    file_queue:FileInfo = _FileQueue()
+
     for file in data.files:
         if optional and file.licenses:
             continue
+        file_queue.put(file)
 
-        result = subprocess.run(['scancode', '-cl',
-                                '--json-pp', '-',
-                                '--license-text',
-                                '--license-text-diagnostics',
-                                '-n', str(cpu_count()),
-                                file.file_path],
-                                capture_output=True,
-                                encoding='utf-8')
+    threads = [_Worker(_run_scancode, file_queue) for _ in range(_cpu_count())]
+    for thread in threads:
+        thread.start()
 
-        try:
-            result.check_returncode()
-        except:
-            file.errors.append(f'scancode_toolkit: {result.stderr}')
-        else:
-            result_dict = json.loads(result.stdout)
-            licenses = set()
-            for i in result_dict['files'][0]['licenses']:
-                if i['key'] == 'unknown-spdx':
-                    licenses.add(i['matched_text'].replace('SPDX-License-Identifier: ', ''))
-                else:
-                    licenses.add(i['name'])
-            file.licenses = file.licenses.union(licenses)
-            file.detectors.add('scancode_toolkit')
+    for _ in threads:
+        file_queue.close()
+    file_queue.join()
+
+    for thread in threads:
+        thread.join()
 
