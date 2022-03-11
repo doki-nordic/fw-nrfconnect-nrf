2fce0fc945c8937967e43489f82deaaee4b4e5cf
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2022-03-11 10:17:03 +0100

    scripts: sbom: Warn about missing input files
    Signed-off-by: Dominik Kilian <Dominik.Kilian@nordicsemi.no>

--- scripts/west_commands/sbom/file_input.py
+++ scripts/west_commands/sbom/file_input.py
@@ -3,14 +3,20 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
+from west import log
 import re
 from typing import Generator
 from pathlib import Path
 from args import args
 from data_structure import Data, FileInfo
+from common import SbomException
 
 
 GLOB_PATTERN_START = re.compile(r'[\*\?\[]')
+
+
+def is_glob(glob: str) -> bool:
+    return GLOB_PATTERN_START.search(glob) is not None
 
 
 def glob_with_abs_patterns(path: Path, glob: str) -> Generator:
@@ -33,9 +39,17 @@ def resolve_globs(path: Path, globs: 'list[str]') -> 'set(Path)':
             for file in glob_with_abs_patterns(path, glob[1:]):
                 result.discard(file)
         else:
+            added_files = 0
             for file in glob_with_abs_patterns(path, glob):
                 if file.is_file():
                     result.add(file)
+                    added_files += 1
+            if added_files == 0:
+                if is_glob(glob):
+                    log.wrn(f'Input glob "{glob}" does not match any file.')
+                else:
+                    log.err(f'Input file "{glob}" does not exists.')
+                    raise SbomException('Invalid input')
     return result
 
 
@@ -54,12 +68,15 @@ def generate_input(data: Data):
             file_path = Path(file).resolve()
             data.inputs.append(f'list of files read from: {file_path}')
             globs = list()
-            with open(file_path, 'r') as fd:
-                for line in fd:
-                    line = line.strip()
-                    if line == '' or line[0] == '#':
-                        continue
-                    globs.append(line)
+            try:
+                with open(file_path, 'r') as fd:
+                    for line in fd:
+                        line = line.strip()
+                        if line == '' or line[0] == '#':
+                            continue
+                        globs.append(line)
+            except Exception as e:
+                raise SbomException(f'Cannot read from file "{file_path}": {e}') from e
             r = resolve_globs(file_path.parent, globs)
             full_set = full_set.union(r)
 
