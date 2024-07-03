1d05f683deec432d8e5b1f7d62e6b38913ba2224
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2024-07-03 23:12:42 +0200

    GPIO fixed 2

--- samples/irq_latency/src/main.c
+++ samples/irq_latency/src/main.c
@@ -6,6 +6,7 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pwm.h>
 #include <zephyr/irq.h>
 #include <zephyr/logging/log.h>
 #include <nrf.h>
@@ -32,14 +33,13 @@ static const struct gpio_dt_spec response2_spec = GPIO_DT_SPEC_GET(RESPONSE2_NOD
 static const struct gpio_dt_spec response3_spec = GPIO_DT_SPEC_GET(RESPONSE3_NODE, gpios);
 static const struct gpio_dt_spec input_spec = GPIO_DT_SPEC_GET(INPUT_NODE, gpios);
 
+static const struct pwm_dt_spec pwm = PWM_DT_SPEC_GET(DT_PATH(zephyr_user));
+
 static int test_gpio(const struct shell *shell, size_t argc,
 			       char **argv)
 {
 	static int i = 1;
-	nrf_gpio_port_out_clear(NRF_P0_S, 1 << (4 + 0));
-	nrf_gpio_port_out_clear(NRF_P0_S, 1 << (4 + 1));
-	nrf_gpio_port_out_clear(NRF_P0_S, 1 << (4 + 2));
-	nrf_gpio_port_out_clear(NRF_P0_S, 1 << (4 + 3));
+	nrf_gpio_port_out_clear(NRF_P0_S, 0xF << 4);
 	gpio_pin_set_dt(&trigger_spec, i & 1);
 	i++;
 	return 0;
@@ -56,10 +56,25 @@ static int test_read(const struct shell *shell, size_t argc, char **argv)
 
 SHELL_CMD_REGISTER(read, NULL, "Get.", test_read);
 
+static int cmd_awake(const struct shell *shell, size_t argc, char **argv)
+{
+	int64_t start = k_uptime_get();
+	while (start + 5000 > k_uptime_get()) {
+		for (int i = 0; i < 1000000; i++) {
+			__asm volatile("");
+		}
+	}
+	return 0;
+}
+
+SHELL_CMD_REGISTER(awake, NULL, "Get.", cmd_awake);
+
 static void interrupt_callback(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
 	//nrf_gpio_port_out_set(NRF_P0_S, 1 << (4 + 3));
 	*(int32_t*)(0x5F938000 + 0x004) = 1 << (4 + 3);
+	for (volatile int i = 0; i < 100; i++);
+	nrf_gpio_port_out_clear(NRF_P0_S, 0xF << 4);
 }
 
 int main(void)
@@ -72,6 +87,16 @@ int main(void)
 	err = dk_leds_init();
 	if (err) {
 		LOG_ERR("LEDs initialization failed, err %d", err);
+		return 0;
+	}
+
+	if (!pwm_is_ready_dt(&pwm)) {
+		printk("Error: PWM device %s is not ready\n", pwm.dev->name);
+		return 0;
+	}
+
+	if (pwm_set_dt(&pwm, PWM_MSEC(100), PWM_MSEC(50))) {
+		LOG_ERR("Pwm led 4 set fails:\n");
 		return 0;
 	}
 
